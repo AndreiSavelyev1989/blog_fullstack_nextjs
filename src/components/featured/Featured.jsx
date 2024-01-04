b6a1d788ba9a3dc22a8821a1ede50bb626e7a0f0
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2024-01-04 14:43:58 +0300

    added layout for featured block

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import styles from "./Featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured!</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>The standard Lorem Ipsum</b> passage, used since the 1500s.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            The standard Lorem Ipsum passage, used since the 1500s
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
